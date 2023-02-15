0da15770a133b5578203116f0e5d601ce15bee6f
SomdebDatta <somdebdatta@gmail.com>
2023-02-15 22:48:02 +0530

    adding docker files

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
 
-api = FastAPI()
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    msg = {
+        "Message": "Hello, welcome to my FastAPI."
+    }
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", port=8000, log_level="info", reload=True)
